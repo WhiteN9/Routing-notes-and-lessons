492674d9f81615a5c18f01ac540f8803aa72b720
White Nguyen <whitenguyen0909@gmail.com>
2022-05-10 14:13:30 -0600

    added link, switch, useLocation for nomatch location

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+
+function Home() {
+  return <p>Home</p>;
+}
+
+function About() {
+  return <p>About</p>;
+}
+
+function AboutSettings() {
+  return <p>About Settings</p>;
+}
+
+// Route is like an `if` statement, matches and renders location inclusively, i.e. any route that has `/about`
+// `exact` means exact={true} but looks like english
+// Link is like anchor+href tag+attribute
+// Switch renders the first child <Route> or <Redirect>,
+//  that matches and renders a route exclusively.
+//  For example, multiple `setting` routes, we might just want 1 setting route
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="container my-3">
+          <h1>My App</h1>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+
+        <main className="container">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/about/settings">
+              <AboutSettings />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+          </Switch>
+        </main>
+      </div>
+    </Router>
   );
 }
 
