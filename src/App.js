91ca83fa06e664aa45d6129a50a7cb10a7e5aade
White Nguyen <whitenguyen0909@gmail.com>
2022-05-10 15:01:34 -0600

    able to render simple page with Link and change url

--- src/App.js
+++ src/App.js
@@ -1,63 +1,36 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
-import NoMatch from "./NoMatch";
+import React from 'react';
+import {Link , Switch, Route} from "react-router-dom";
 
 function Home() {
-  return <h1>Welcome to the home page</h1>;
+  return <h1>Welcome to the home page</h1>
 }
 function About() {
-  return <h1>You are on the about page</h1>;
+  return <h1>You are on the about page</h1>
 }
 function Contact() {
-  return <h1>Please feel free to email us</h1>;
+  return <h1>Please feel free to email us</h1>
 }
-function AboutSettings() {
-  return <p>About Settings</p>;
+function NoMatch() {
+  return <h1>404 Not Found</h1>
 }
 
-// Route is like an `if` statement, matches and renders location inclusively, i.e. any route that has `/about`
-// `exact` means exact={true} but looks like english
-// Link is like anchor+href tag+attribute
-// Switch renders the first child <Route> or <Redirect>,
-//  that matches and renders a route exclusively.
-//  For example, multiple `setting` routes, we might just want 1 setting route
-
-function App() {
+function App () {
   return (
-    <Router>
-      <div className="App">
-        <header className="container my-3">
-          <h1>My App</h1>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-            </ul>
-          </nav>
-        </header>
-
-        <main className="container">
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/about/settings">
-              <AboutSettings />
-            </Route>
-            <Route path="/about">
-              <About />
-            </Route>
-            <Route>
-              <NoMatch />
-            </Route>
-          </Switch>
-        </main>
-      </div>
-    </Router>
+    // No need to add <Router>, it has been added to ./index.js
+  <>
+    <div className="App">
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/contact">Contact</Link>
+    </div>
+    <Switch>
+      <Route exact path="/"><Home /></Route>
+      <Route exact path="/about"><About /></Route>
+      <Route exact path="/contact"><Contact /></Route>
+      <Route><NoMatch /></Route>
+    </Switch>
+  </>
+    
   );
 }
 
