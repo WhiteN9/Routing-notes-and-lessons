7a92128f997722a8ad8aac8ec883910a8bf15890
White Nguyen <whitenguyen0909@gmail.com>
2022-05-10 20:10:48 -0600

    25-4 app and userprofile

--- src/App.js
+++ src/App.js
@@ -1,84 +1,43 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
-import NoMatch from "./NoMatch";
-import UserProfile from "./UserProfile";
+import { Link, Route, Switch } from "react-router-dom";
+import UserProfile from "./UserProfile.js";
 
-function Home() {
-  return <h1>Welcome to the home page</h1>;
+function NewUser() {
+  return <p>Unable to create a new user</p>
 }
-function AboutSettings() {
-  return <h1>About Settings</h1>;
+function DefinitelyNoMatch() {
+  return <h1>404 Not Found</h1> 
 }
-function About() {
-  return <h1>You are on the about page</h1>;
-}
-function Contact() {
-  return <h1>Please feel free to email us</h1>;
-}
-
-// Route is like an `if` statement, matches and renders location inclusively, i.e. any route that has `/about`
-// `path` property describes the URL this route will handle
-// `exact` means exact={true} but looks like english
-// Link is like anchor+href tag+attribute, often points to a route
-// Switch renders the first child <Route> or <Redirect>,
-//  that matches and renders a route exclusively.
-//  For example, multiple `setting` routes, we might just want 1 setting route
 
 function App() {
   return (
-    <>
-      <div className="App">
-        <header className="container my-3">
-          <h1>My App</h1>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about-settings">About Settings</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <Link to="/contact">Contact</Link>
-              </li>
-            </ul>
-          </nav>
-        </header>
-        {Array(10) //Create an array of 10 empty items [ <10 empty items> ]
-          .fill() //Fill the array items with undefined [undefined,undefined,...]
-          .map((ignoreValue, index) => index + 1) //Make a new array with map that has indices as values[1,2...10]
-          .map((id) => (//Create 10 `Link to` items on the page
-            <div key={id}>
-              <Link to={`/user/${id}`}>User {id}</Link>
-            </div>
-          ))}
-        <main className="container">
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/about-settings">
-              <AboutSettings />
-            </Route>
-            <Route path="/about">
-              <About />
-            </Route>
-            <Route path="/contact">
-              <Contact />
-            </Route>
-            <Route path="/user/:userId">
-              <UserProfile />
-            </Route>
-            <Route>
-              <NoMatch />
-            </Route>
-          </Switch>
-        </main>
-      </div>
-    </>
+    // No need to add <Router>, it has been added to ./index.js
+    <div className="App">
+      <Link to="/user/new">New User</Link>
+      {Array(10)
+        .fill()
+        .map((ignoredValue, index) => index + 1)
+        .map((id) => (
+          <div key={id}>
+            <Link to={`/user/${id}`} data-testid={`user-${id}`}>
+              User{id}
+            </Link>
+          </div>
+        ))}
+      <main>
+        <Switch>
+          <Route exact path="/user/:userId">
+            <UserProfile />
+          </Route>
+          <Route exact path="/user/new">
+            <UserProfile />
+          </Route>
+          <Route>
+            <DefinitelyNoMatch />
+          </Route>
+        </Switch>
+      </main>
+    </div>
   );
 }
 
--- src/UserProfile.js
+++ src/UserProfile.js
@@ -1,41 +1,38 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-//useParams hook returns an object of key-value pairs of route parameters
 function UserProfile() {
   const [user, setUser] = useState({});
+  const { userId } = useParams();
 
-  // Use `useParams()` and `useEffect)`
-  // Load profile data from https://jsonplaceholder.typicode.com/users/${userId}
-
-  const {userId} = useParams(); //or   const userId = useParams().userId;
-
-  const controller = new AbortController();
   useEffect(() => {
-    setUser({})
-    async function getProFileData() {
+    const abortController = new AbortController();
+
+    async function loadUser() {
       try {
-        {
-          const response = await fetch(
-            `https://jsonplaceholder.typicode.com/users/${userId}`,
-            { signal: controller.signal }
-          );
-          const profileData = await response.json();
-          setUser(profileData);
-        }
+        const response = await fetch(
+          `https://jsonplaceholder.typicode.com/users/${userId}`,
+          { signal: abortController.signal }
+        );
+
+        const user = await response.json();
+        setUser(user);
       } catch (error) {
-        console.log(error.name);
+        if (error.name !== "AbortError") {
+          console.error(error);
+        }
       }
     }
 
-    getProFileData();
+    loadUser();
 
-    return function cleanup() {
-      controller.abort();
+    return () => {
+      abortController.abort(); // cancels any pending request or response
     };
-  }, [userId]); //important, this one is the one that is changing
+  }, [userId]);
 
-  // No need to change anything below here
+  // You do not need to change this component
+
   if (user.id) {
     return Object.entries(user).map(([key, value]) => (
       <div key={key}>
@@ -44,24 +41,7 @@ function UserProfile() {
       </div>
     ));
   }
-  return "Loading...";
+  return "Unable to create a new user";
 }
 
 export default UserProfile;
-
-// if path="/users/:userId/post/:postId
-
-// function UserProfile() {
-//   const params = useParams();
-//   return <p>{JSON.stringify(params)}</p>;
-//   // >>> {"userId":"params-can-be-anything","postId":"{even-things-that-look-like-JSON:\"value:}"}
-// }
-
-// function UserProfile() {
-//   const params = useParams();
-//   console.log(params); //
-//   // http://localhost:3000/users/1/post/1 , run this link
-//   // console.log will give {userId: '1', postId: '1'} and
-//   return <p1>{params.userId} - {params.postId}</p1>;
-//   // >> 1 - 1 , this will be displayed on page
-// }
